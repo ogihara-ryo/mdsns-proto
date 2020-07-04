d50e8b52ba52fc7739d213d20ee24c2ebec4df62
Ogihara Ryo <ryo@ryukalice.com>
2020-07-05 02:07:39 +0900

    プロフィールページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get ':nickname' => 'users#show'
 
   get '/auth/github/callback' => 'sessions#create'
   delete 'sign_out', to: 'sessions#destroy'
